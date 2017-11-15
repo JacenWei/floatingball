25bddef80da7a176d698a37dd2799f71b93527fa
weijx <a553221632@163.com>
2017-11-15 15:08:14 +0800

    zhengli

--- app/src/main/java/com/demo/floatingball/MainActivity.java
+++ app/src/main/java/com/demo/floatingball/MainActivity.java
@@ -1,13 +1,11 @@
 package com.demo.floatingball;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
 import floatwindow.xishuang.float_lib.FloatActionController;
-import floatwindow.xishuang.float_lib.permission.FloatPermissionManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        boolean isPermission = FloatPermissionManager.getInstance().applyFloatWindow(this);
+//        boolean isPermission = FloatPermissionManager.getInstance().applyFloatWindow(this);
         //有对应权限或者系统版本小于7.0
-        if (isPermission || Build.VERSION.SDK_INT < 24) {
+//        if (isPermission || Build.VERSION.SDK_INT < 24) {
             //开启悬浮窗
             FloatActionController.getInstance().startMonkServer(this);
-        }
+//        }
     }
 
     @Override
